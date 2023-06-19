368ebdce704d3d0ca6cad8da4ec3c30ef6f5a648
jorge871 <jg303114@gmail.com>
2023-06-19 12:52:22 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	{
+	putchar(low);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
