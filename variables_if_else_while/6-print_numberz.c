7e1e59f382708c84faee903c5e1f512d222b9d20
jorge871 <jg303114@gmail.com>
2023-06-19 11:24:59 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number ++)
+	{
+		putchar(number + 0);
+	}
+	putchar('\n');
+	return (0);
+}
