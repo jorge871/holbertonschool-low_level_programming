42fce9dde189cf564264718aee203109eaafb16f
jorge871 <jg303114@gmail.com>
2023-06-19 13:52:02 -0700

    task 9

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+char c;
+while (c != 'g')
+{
+if (c <= '9')
+{
+for (c = '0'; c <= '9'; ++c)
+putchar(c);
+}
+else
+{
+c = 'a';
+while (c != 'g')
+{
+putchar(c);
+++c;
+}
+}
+}
+putchar('\n');
+return (0);
+}
