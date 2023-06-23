33b048fea17ffbf70c257b44cf056d3dfc305392
jorge871 <jg303114@gmail.com>
2023-06-22 17:41:01 -0700

    task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,12 +3,12 @@
 void
 _puts(char *str)
 {
-int cont = 0;
-while (str[cont] != '\0')
-{
-_putchar(str[cont]);
-cont++;
-}
-
+	int cont = 0;
+	while (*(str + cont))
+	{
+		_putchar(*(str + cont);
+		cont++;
+	}
+		_putchar('\n');
 
 }
