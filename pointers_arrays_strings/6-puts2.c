500d79f475e62011878b81a250f6a134b76a1ce2
jorge871 <jg303114@gmail.com>
2023-06-23 12:01:30 -0700

    task 5

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+void
+puts2(char *str)
+{
+	int k = 0;
+	if (str == NULL)
+ 	return;
+	
+	while (str[k] != '\0')
+{
+	putchar(str[k]);
+	k += 2;
+}
+
+	putchar('\n');
+}
