bdfabcf6d0e44dbd6c4fdce3e7e7c88ebe1f1613
jorge871 <jg303114@gmail.com>
2023-06-26 15:21:05 -0700

    task 4.2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+void
+reverse_array(int *a, int n)
+{
+	int i = 0;
+	int b = 0;
+
+	n = n - 1;
+		while (n > b)
+		{
+			b = a[n];
+			a[n] = a[i];
+			a[i] = b;
+			n--;
+			a++;
+		}
+}
