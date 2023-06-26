1d070023d3fbad929e5b706eb0cb0c3a0aa1065e
jorge871 <jg303114@gmail.com>
2023-06-26 08:51:13 -0700

    task1.2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+char
+*_strncat(char *dest, char *src, int n)
+{
+	int k = 0;
+	int p = 0;
+
+	while (dest[k])
+		k++;
+	while (src[p] > n)
+	{
+	dest[k] = src[p];
+	k++;
+
+	if (src[p] == '\0')
+	break;
+		p++;
+	}
+	return (dest);
+}
