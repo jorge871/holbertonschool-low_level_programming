bec8b6e8e9b3b71c820fcac19e9a1a53f96bb124
jorge871 <jg303114@gmail.com>
2023-06-26 17:36:38 -0700

    task 6.2

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,33 +3,33 @@
 char
 *cap_string(char *cap)
 {
-	int cont = 0;
+int cont = 0;
 
-	while (cap[cont])
-	{
-		if (cap[cont] >= 97 && (cap[cont] <= 122 && cap[cont] == cap[0]))
-			cap[cont] = cap[cont] - 32;
-		if (cap[cont] >= 97 && cap[cont] <= 122)
-		{
-			switch (cap[cont - 1])
-			{
-				case 32:
-				case 44:
-				case 59:
-				case 46:
-				case 10:
-				case 9:
-				case 33:
-				case 63:
-				case 34:
-				case 40:
-				case 41:
-				case 123:
-				case 125:
-					cap[cont] = cap[cont] - 32;
-			}
-		}
-		cont++;
-	}
-	return (cap);
+while (cap[cont])
+{
+if (cap[cont] >= 97 && (cap[cont] <= 122 && cap[cont] == cap[0]))
+cap[cont] = cap[cont] - 32;
+if (cap[cont] >= 97 && cap[cont] <= 122)
+{
+switch (cap[cont - 1])
+{
+case 32:
+case 44:
+case 59:
+case 46:
+case 10:
+case 9:
+case 33:
+case 63:
+case 34:
+case 40:
+case 41:
+case 123:
+case 125:
+cap[cont] = cap[cont] - 32;
+}
+}
+cont++;
+}
+return (cap);
 }
