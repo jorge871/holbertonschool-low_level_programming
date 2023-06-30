33bd8c9d4e1867dabfbf9f9d3d092896f5e24040
jorge871 <jg303114@gmail.com>
2023-06-30 10:35:47 -0700

    task 5 of Recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 int
 _sqrt_recursion(int n)
 {
@@ -6,18 +7,18 @@ _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	return (_sqrt_recursion(n - 1));
-
-	{
-	int a, s;
-
-	if (s * s < a)
-	{
-	return (-1);
-	}
+	return (_sqrt_re(n, 0));
 }
+int
+_sqrt_re(int u, int d)
+{
+	if (d * d == u)
+	{
+		return (d);
+	}
+	if (d * d > u)
+	{
+		return (-1);
+	}
+	return (_sqrt_re(u, d + 1));
 }
