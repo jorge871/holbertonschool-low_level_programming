6fc8544eb10e572d45fc926160389e5ab3ba965f
jorge871 <jg303114@gmail.com>
2023-07-09 18:25:39 -0700

    task of argc/argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,9 +3,12 @@
 int
 main(int argc, char *argv[])
 {
+	int count;
 
-	if (argv[0])
-		printf("%d\n", argc - 1);
+	printf("%s\n", argv[0]);
+	if (argc > 1)
+		for (count = 1; count < argc; count++)
+			printf("argv[%d] = %s\n", count, argv[count]);
 	return (0);
 }
 
