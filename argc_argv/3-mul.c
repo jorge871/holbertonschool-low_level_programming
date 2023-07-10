059341bb4ace275d4cd98d58ac98b216e3695fee
jorge871 <jg303114@gmail.com>
2023-07-09 18:33:25 -0700

    task of argc/argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int
+main(int argc, char *argv[])
+{
+	if (argc < 4 && argc > 2)
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
