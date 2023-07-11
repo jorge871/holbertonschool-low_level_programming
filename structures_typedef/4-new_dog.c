614f88d9621c1674dbb05acf45da4bbce2495850
jorge871 <jg303114@gmail.com>
2023-07-11 10:42:23 -0700

    task of structures

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *new_dog - Create a new dog.
+ *@name: name of dog.
+ *@age: age of dog.
+ *@owner: owner of the dog.
+ *return: return this new dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+	unsigned int cont;
+
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	my_dog->name = malloc(strlen(name) + 1);
+		if (my_dog->name == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->name = malloc(strlen(owner) + 1);
+		if (my_dog->owner == NULL)
+		{
+			free(my_dog->owner);
+			free(my_dog->name);
+			free(my_dog);
+			return (NULL);
+		}
+		for (cont = 0; cont < strlen(name); cont++)
+			my_dog-name
+
+
+}
